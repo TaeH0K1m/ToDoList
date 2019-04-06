336600d61c51576fc6d6dc61d8e960ca06bdd974
TaeH0K1m <taeh0k1m.swe@gmail.com>
2019-04-06 00:07:29 -0700

    Add basic Todo form

--- app.py
+++ app.py
@@ -2,12 +2,24 @@ from flask import Flask
 from flask import render_template
 from flask_bootstrap import Bootstrap
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class TodoForm(FlaskForm):
+    todo_item = StringField('todo_item', validators=[DataRequired()])
+    submit_button = SubmitField('Add item')
+
 app = Flask(__name__)
+# in a real app, these should be configured through Flask-Appconfig
+app.config['SECRET_KEY'] = 'devkey'
+
 Bootstrap(app)
 
 @app.route('/')
 def hello():
-    return render_template("index.html") 
+    form = TodoForm()
+    return render_template("index.html", form=form)
 
 if __name__ == "__main__":
 	app.run(debug=True)
